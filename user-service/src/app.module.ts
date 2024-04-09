588f1e8e67af04844713c2c87233641e4d5ebc63
Jsosé Lucas <lukasalvespod1@gmail.com>
2024-04-09 16:04:36 -0300

    feat: add consumer kafka in user service

--- user-service/src/app.module.ts
+++ user-service/src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
-import { PrismaService } from './prisma/prisma.service';
-import { S3Service } from './s3/s3.service';
 
 @Module({
   imports: [UserModule],
-  providers: [PrismaService, S3Service],
 })
 export class AppModule {}
--- user-service/src/user/user.module.ts
+++ user-service/src/user/user.module.ts
@@ -5,9 +5,24 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from 'src/jwt/jwt.strategy';
 import { S3Service } from 'src/s3/s3.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [PassportModule.register({ defaultStrategy: 'jwt' })],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    ClientsModule.register([
+      {
+        name: 'USER_SERVICE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: 'users',
+            brokers: ['localhost:9092'],
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [UserController],
   providers: [UserService, PrismaService, JwtStrategy, S3Service],
 })
